01eb7ec3fa61aed4dad24215961970617027b03c
YevheniiaHr <131554616+YevheniiaHr@users.noreply.github.com>
2023-12-23 17:12:51 +0100

    task 4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,49 @@
+class Key {
+    private signature: number 
+    constructor(){
+        this.signature = Math.random();
+    }
+    getSignature(): number{
+        return this.signature;
+    }
+}
+class Person{
+    constructor(private key: Key){
+        this.key;
+    };
+    getKey():Key{
+        return this.key;
+    }
+}
+abstract class House {
+    constructor(protected door: boolean,
+        protected key: Key, 
+        protected tenants: Person[]=[])
+    {
+        this.door =false;
+        this.key =key;
+
+    }
+    abstract openDoor(key: Key): void;
+    comeIn(person: Person): void{
+        if(this.door){
+            this.tenants.push(person);
+
+        }else{
+            console.log("Door closed")
+        }
+    };
+};
+ 
+class MyHouse extends House{
+    openDoor(key: Key): void {
+        if(key.getSignature() === this.key.getSignature()){
+            this.door=true;
+        }else{
+            console.log("Door closed")
+        }
+    }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
